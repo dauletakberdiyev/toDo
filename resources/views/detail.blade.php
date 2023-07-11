7b33f2fda793d4a1010e48ccaf775cdcd3e1c9a3
HahhVwgwg <serikzhan.parmanov@gmail.com>
2023-07-11 23:38:54 +0600

    preview/open img done

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -14,8 +14,23 @@
         <div class="card-body">
             <h5 class="card-title">{{$todo->name}}</h5>
             <p class="card-text">{{$todo->desc}}</p>
+            <div class="d-flex w-100 justify-content-center">
+                <img 
+                    class="img-fluid" 
+                    src="{{URL::to('/')}}/images/{{$todo->picture}}" 
+                    width="150" height="150"
+                    onclick="onClickImg(this.currentSrc)"
+                >
+            </div>
             <a href="edit/{{$todo->id}}"><span class="btn btn-primary">Edit</span></a>
             <a href="delete/{{$todo->id}}"><span class="btn btn-danger">Delete</span></a>
         </div>
 </div>
+@endsection
+@section('js')
+    <script type="text/javascript">
+        function onClickImg($src){
+            window.open($src, '_blank')
+        }
+    </script>
 @endsection
